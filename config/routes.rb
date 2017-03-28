5489c8a8f73ec3225326297e0cb01ba30446dab4
Rodrigo Gonçalves <rgoncalves@esec.com.br>
2017-03-28 17:30:32 -0300

    Scaffold team

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :teams
   resources :kids
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'kids#index'
+
 end
